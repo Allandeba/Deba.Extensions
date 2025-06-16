43ff963304257415b1f04df600ce61fe4154b2c9
Allan Debastiani <allandeba@yahoo.com.br>
2025-06-16 20:59:23 -0300

    feat: enumerable helpers

--- Deba.Extensions/EnumExtensions.cs
+++ Deba.Extensions/EnumExtensions.cs
@@ -4,18 +4,47 @@ namespace Deba.Extensions;
 
 public static class EnumExtensions
 {
-    public static int GetValue<T>(this T enumType) where T : Enum =>
+    public static int GetValue<TEnum>(this TEnum enumType) where TEnum : struct, Enum =>
         Convert.ToInt32(enumType);
 
     public static string GetDescription(this Enum enumValue)
     {
         var enumAsString = enumValue.ToString();
-        
+
         var field = enumValue.GetType().GetField(enumAsString);
         if (field is null) return enumAsString;
-        
+
         var attributes = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
         return attributes is null ? enumAsString : attributes.Description;
     }
+
+    public static TEnum GetValue<TEnum>(string enumDescription) where TEnum : struct, Enum
+    {
+        var type = typeof(TEnum);
+        foreach (var field in type.GetFields())
+        {
+            if (!field.IsStatic)
+                continue;
+
+            var descriptionAttribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            var description = descriptionAttribute is not null
+                ? descriptionAttribute.Description
+                : field.Name;
+
+            if (string.Equals(description, enumDescription, StringComparison.InvariantCultureIgnoreCase)
+                || string.Equals(field.Name, enumDescription, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return (TEnum)field.GetValue(null)!;
+            }
+        }
+
+        throw new ArgumentException($"Nenhum valor do enum {type.Name} com descrição '{enumDescription}' foi encontrado.", nameof(enumDescription));
+    }
     
+    public static bool IsEqualTo<TEnum>(this string enumDescription, TEnum enumValue) where TEnum : struct, Enum
+    {
+        var parsedValue = GetValue<TEnum>(enumDescription);
+        return EqualityComparer<TEnum>.Default.Equals(parsedValue, enumValue);
+    }
 }
